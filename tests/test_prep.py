cbcf87388a44ac62001b2e4978faa7f81858565f
Gwilym-Rutherford <gmarutherford1@sheffield.ac.uk>
2026-03-15 20:23:43 +0000

    Complete test_prep coverage

--- tests/test_prep.py
+++ tests/test_prep.py
@@ -8,4 +8,32 @@ class MyTestCase(unittest.TestCase):
             second='behaviour 3'
         )
 
-    # TODO: Can you write more test cases below to increase the test coverage of `strange_function`?
+    def test_get_behaviour1(self):
+        self.assertEqual(
+            first=strange_function(1, 1, 3, 4),
+            second='behaviour 1'
+        )
+
+    def test_get_behaviour2(self):
+        self.assertEqual(
+            first=strange_function(1, 1, 4, 3),
+            second='behaviour 2'
+        )
+
+    def test_get_behaviour4(self):
+        self.assertEqual(
+            first=strange_function(4, 3, 1, 1),
+            second='behaviour 4'
+        )
+
+    def test_get_behaviour5(self):
+        self.assertEqual(
+            first=strange_function(4, 2, 1, 4),
+            second='behaviour 5'
+        )
+
+    def test_get_behaviour6(self):
+        self.assertEqual(
+            first=strange_function(4, 2, 4, 4),
+            second='behaviour 6'
+        )

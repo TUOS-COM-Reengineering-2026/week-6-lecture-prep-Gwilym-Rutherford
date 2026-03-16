51fcde39b10291de3b94dcdf419969a83af437b6
Gwilym-Rutherford <gmarutherford1@sheffield.ac.uk>
2026-03-16 12:21:07 +0000

    Add second test for difficult_function

--- tests/test_difficult_function.py
+++ tests/test_difficult_function.py
@@ -1,11 +1,17 @@
 import unittest
 
-from unittest.mock import MagicMock, patch
-from lecture import difficult_function, complex_math
+from unittest.mock import patch
+from lecture import difficult_function
 
 class MyTestCase(unittest.TestCase):
     @patch('lecture.complex_math')
-    def test_difficult_function(self, complex_math):
-        complex_math.return_value = 0
+    def test_difficult_function_1(self, complex_math_mock):
+        complex_math_mock.return_value = 0
         self.assertEqual(first="solved!",
+                         second=difficult_function(1,2))
+
+    @patch('lecture.complex_math')
+    def test_difficult_function_2(self, complex_math_mock):
+        complex_math_mock.return_value = 1
+        self.assertEqual(first="not yet",
                          second=difficult_function(1,2))
